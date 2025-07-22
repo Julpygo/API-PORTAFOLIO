9ef664e41143e76c238920ea09e136f6f904ab2c
Julpygo <gomez.julian@correounivalle.edu.co>
2025-07-21 21:34:37 -0500

    fin de clase

--- models/proyectos.js
+++ models/proyectos.js
@@ -5,7 +5,27 @@ const Schema = mongoose.Schema
 const ProyetoSchema = new Schema({
     nombre : {
         type : String,
-        required:true,
-        maxLength:100
+        required : true,
+        maxLength : 100
+    },
+    imagen : {
+        type: String,
+        required : true,
+        maxLength : 100
+    },
+        repo : {
+        type : String,
+        required : true,
+        maxLength : 100
+    },
+    tecnologias : {
+        type: [String],
+    },
+    descripcion : {
+        type: String,
+        required : true,
+        maxLength : 500
     }
 })
+
+module.exports = mongoose.model("Proyecto",ProyetoSchema)
