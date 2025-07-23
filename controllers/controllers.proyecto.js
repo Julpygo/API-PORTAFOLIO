92885f69af25fd69565e0a010092116859f49a02
Julpygo <gomez.julian@correounivalle.edu.co>
2025-07-22 20:56:05 -0500

    fin de clase

--- controllers/controllers.proyecto.js
+++ controllers/controllers.proyecto.js
@@ -1,4 +1,31 @@
+const ProyectosModel = require("../models/proyectos")
 exports.Hola = (req,res)=>{
     console.log("hola desde el controlador")
     res.send("hola desde el controlador")
+}
+exports.proyectosList = async (req,res)=>{
+    try {
+        const proyectosList = await ProyectosModel.find({})
+        return res.status(200).json(proyectosList)
+    } catch (error) {
+        return res.status(500).send(error)
+    }
+}
+exports.crearProyecto = async (req,res) =>{
+    try {
+        const proyecto = req.body
+        await ProyectosModel.create(proyecto)
+        return res.status(201).json(proyecto)  
+    } catch (error) {
+        return res.status(500).send(error)
+    }
+}
+exports.obtenerProyectoPorId = async (req,res)=>{
+    try {
+        const {id} = req.params
+        const proyecto = await ProyectosModel.findById(id)
+        return res.status(200).json(proyecto)
+    } catch (error) {
+        return res.status(500).send(error)
+    }
 }
